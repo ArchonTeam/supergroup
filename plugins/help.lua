9d8cae6a01cdc4aa00763ad8f1f54e04a67c97dc
Archon Team <rezafardipour2@gmail.com>
2016-04-20 19:22:43 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,95 +1,95 @@
 do
     
 function run(msg, matches)
-  local help = [[Self-Bot Commands
+  local help = [[Soft_TG Commands
 
-●#bot on
+!bot on
 فعال کردن بوت در یک گروه خواص
 
-●#bot off
+!bot off
 غیر فعال کردن بوت در یک گروه خواض
 
-●#plugins
+!plugins
 مشاهده لیست پلاگین ها
 
-●#plugins enable (plugin name)
+!plugins enable (plugin name)
 فعال کردن پلاگینی با نام (plugin name)
 
-●#plugins disable (plugin name)
+!plugins disable (plugin name)
 غیر فعال کردن پلاگینی با نام (plugin name)
 
-●#plugins reload
+!plugins reload
 آپدیت کردن لیست پلاگین ها
 
-●#plugins disable (name) gp
+!plugins disable (name) gp
 غیر فعال کردن پلاگین (name) در گروه مورد نظر
 
-●#plugins disable (name) gp
+!plugins disable (name) gp
 فعال کردن پلاگین (name) در گروه مورد نظر
 
-●#addplug (text) (name)
+!addplug (text) (name)
 اضافه کردن پلاگینی به محتوای (text)و نام (name) به لیست پلاگین 
 
-●#getplug (name)
+!getplug (name)
 ارسال پلاگین با نام (name)
 
-●#setname (name)
+!setname (name)
 تغیر نام گروه به (name)
 
-●#link 
+!link 
 دریافت لینک گروه در پیوی
 
-●#newlink
+!newlink
 ساخت لینک جدید
 
-●#tosuper
+!tosuper
 تبدیل گروه معمولی به سوپر گوه
 
-●#setdes (text)
+!setdes (text)
 تغیر دسکریپشن گروه به (text)
 
-●#rmv @username (by reply)
+!kick @username (by reply)
 اخراج فردی با آیدی @username (حتی با ریپلای)
 
-●#add @username (by reply)
+!add @username (by reply)
 ادد کردن فردی با آیدی @username به گروه (حتی با ریپلای)
 
-●#id @username (by reply)
+!id @username (by reply)
 دریافت آیدی عددی فردی با آیدی @username (حتی با ریپلای)
 
-●#gid
+!gid
 دریافت آیدی گروه
 
-●#google (text)
+!google (text)
 جستجو (text) در گوگل
 
-●#webshot (http://google.com)
+!webshot (http://google.com)
 دریافت اسکرین شات از سایت گوگل
 
-●#voice (text)
+!voice (text)
 تبدیل (text) به صدا
 
-●#insta (insta id)
+!insta (insta id)
 دریافت اطلاعات اینستاگرام فردی با آیدی (intsa id)
 
-●#insta (post link)
+!insta (post link)
 دریافت اطلاعات پستی در اینستاگرام با لینک (post link)
 
-●#set (text1) (text2)
+!set (text1) (text2)
 سیو شدن (text2) در جواب (text)
 
-●#get (text1)
+!get (text1)
 ارسال متن سیو شده برای (text1) یعنی (text2)
 
-●#weather (city)
+!weather (city)
 دریافت اطلاعات آب و هوای شهر (city)
 
-●#sticker (text)
+!sticker (text)
 تبدیل (text) به استیکر
 
 .......................................
 📡
-- @BeatBot_Team
+- @Sofy_TG
 .......................................]]
     if matches[1] == 'help' and is_sudo(msg) then
       send_large_msg("user#id"..msg.from.id, help)      
@@ -99,7 +99,8 @@ end
 
 return {
   patterns = {
-    "^#(help)$"
+    "^!(help)$",
+    "^!(Help)$"
   },
   run = run
 }
